ecf2712975cf70f24d234fcdeb2ef69314d796d7
Daniela Wellisz <daniela.wellisz@gmail.com>
2012-06-30 00:34:28 -0700

    added factorial method

--- lib/basic_recursive_methods.rb
+++ lib/basic_recursive_methods.rb
@@ -1,5 +1,11 @@
 module BasicRecursiveMethods
   def fib(n)
+    raise ArgumentError if n < 0
     n > 1 ? fib(n-1) + fib(n-2) : n
+  end
+
+  def factorial(n)
+    raise ArgumentError if n < 0
+    n > 1 ? n * factorial(n-1) : n
   end
 end
--- spec/basic_recursive_methods_spec.rb
+++ spec/basic_recursive_methods_spec.rb
@@ -1,15 +1,28 @@
 require 'basic_recursive_methods'
-include BasicRecursiveMethods
 
-describe "#fib(n)" do
-  it "should return 0 if n = 0 and 1 if n = 1" do
-    fib(0).should == 0
-    fib(1).should == 1
+describe 'basic recursive methods' do
+  include BasicRecursiveMethods
+
+  describe "#fib(n)" do
+    it "should return 0 if n = 0 and 1 if n = 1" do
+      fib(0).should == 0
+      fib(1).should == 1
+    end
+    it "should return fib(n-1) + fib(n-2) if n > 1" do
+      20.times do |n|
+        n += 2
+        fib(n).should == fib(n-1) + fib(n-2)
+      end
+    end
   end
-  it "should return fib(n-1) + fib(n-2) if n > 1" do
-    20.times do |n|
-      n += 2
-      fib(n).should == fib(n-1) + fib(n-2)
+
+  describe 'factorial(n)' do
+    it 'should work' do
+      factorial(0).should == 0
+      factorial(1).should == 1
+      factorial(2).should == 2
+      factorial(3).should == 6
+      factorial(4).should == 24
     end
   end
 end
